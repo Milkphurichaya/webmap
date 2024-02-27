5540eab17701666da7d00133cd058feb26b5a49f
Milkphurichaya <161519916+Milkphurichaya@users.noreply.github.com>
2024-02-28 03:27:19 +0700

    upload webmap

---
+++ data/_9.js
@@ -0,0 +1,1 @@
+var json__9 = {"type":"FeatureCollection","name":"_9","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"village":"จุดที่เหมาะสำหรับสร้างหมู่บ้านจัดสรร"},"geometry":{"type":"Point","coordinates":[98.971822919030586,18.799539320268011]}}]}
